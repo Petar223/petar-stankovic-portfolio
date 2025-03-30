bfab00d60f165ecd81c68393c0645154e123b98b
Petar Stankovic <stankovicpetar23@gmail.com>
2025-03-30 09:54:29 +0200

    Updates

---
+++ static/js/682.5fd75490.chunk.js
@@ -0,0 +1,73 @@
+"use strict";(self.webpackChunkpetar_stankovic_portfolio=self.webpackChunkpetar_stankovic_portfolio||[]).push([[682],{682:(e,i,t)=>{t.r(i),t.d(i,{default:()=>w});t(43);var n=t(464),o=t(64),s=t(939),r=t(809),l=t(158),d=t(767),p=t(614),a=t(644),c=t(579);const x=n.Ay.section``,f=n.Ay.div`
+  padding: 20px;
+`,h=n.Ay.ul`
+  list-style: none;
+  position: relative;
+`,m=n.Ay.li`
+  font-size: 16px;
+  color: #c5c6c9;
+  padding-bottom: 20px;
+  position: relative;
+  padding-left: 30px;
+
+  @media (max-width: 768px) {
+    font-size: 14px;
+  }
+
+  @media (min-width: 769px) and (max-width: 1024px) {
+    font-size: 15px;
+  }
+
+  &::before {
+    content: '';
+    position: absolute;
+    left: 10px;
+    top: 3px;
+    width: 10px;
+    height: 10px;
+    background-color: #c5c6c9;
+    border-radius: 50%;
+    z-index: 1;
+  }
+
+  &:not(:last-child)::after {
+    content: '';
+    position: absolute;
+    left: 14px;
+    top: 13px;
+    height: calc(100% - 10px);
+    width: 1px;
+    background-color: #c5c6c9;
+    z-index: 0;
+  }
+`,j=n.Ay.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: 1px solid #ffffff;
+  padding-bottom: 10px;
+  margin-bottom: 10px;
+`,u=n.Ay.h3`
+  font-size: 20px;
+  color: #ffffff;
+  margin: 0;
+`,b=n.Ay.div`
+  padding: 20px 0 0 0;
+`,g=n.Ay.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 10px;
+`,y=n.Ay.span`
+  font-size: 14px;
+`,A=(0,n.Ay)(a.nw)`
+  width: 16px;
+  height: 16px;
+  fill: #ffffff;
+  transition: transform 1s ease-in-out;
+
+  &:hover {
+    fill: #4a90e2;
+  }
+`;const w=function(e){let{data:i,showCompany:t=!1,showRoles:n=!1,showLink:a=!1}=e;return(0,c.jsx)(x,{children:i.map(((e,i)=>(0,c.jsxs)(f,{children:[t&&(0,c.jsxs)(j,{children:[(0,c.jsxs)(u,{children:[e.company," - ",e.location]}),(0,c.jsx)(y,{children:e.duration})]}),n&&e.roles&&e.roles.map(((e,i)=>(0,c.jsxs)(o.A,{children:[(0,c.jsxs)("strong",{children:[e.title,":"]})," ",e.start_date," -"," ",e.end_date]},i))),e.projects.map(((e,i)=>(0,c.jsxs)(b,{children:[(0,c.jsxs)(g,{children:[(0,c.jsxs)(d.A,{children:[e.name,a&&e.link&&(0,c.jsx)(s.A,{href:e.link,target:"_blank",rel:"noopener noreferrer",children:(0,c.jsx)(A,{})})]}),e.duration&&(0,c.jsx)(y,{children:e.duration})]}),e.description&&(0,c.jsx)(o.A,{children:e.description}),(0,c.jsx)(l.A,{children:e.technologies.map(((e,i)=>{const t=p.A[e];return(0,c.jsxs)(r.A,{children:[t&&(0,c.jsx)(t,{}),e]},i)}))}),e.responsibilities&&(0,c.jsx)(h,{children:e.responsibilities.map(((e,i)=>(0,c.jsx)(m,{children:e},i)))})]},i)))]},i)))})}}}]);
+//# sourceMappingURL=682.5fd75490.chunk.js.map
